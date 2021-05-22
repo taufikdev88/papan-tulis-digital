8361ac4c8c7d98a343e11ed8744000dc62be47a2
Taufik Hidayat <taufikdev88@gmail.com>
2021-05-22 20:01:09 +0700

    update

--- pythonfiles/raspi.sh
+++ pythonfiles/raspi.sh
@@ -4,3 +4,14 @@ sudo apt install python3-pyqt5 -y
 pip3 install asyncqt
 pip3 install Pillow
 curl -s https://install.zerotier.com | sudo bash
+sudo zerotier-cli join 93afae59639d273c
+
+# installasi di raspberry pi
+# pindah di folder /home/pi dan rename agar gampang
+# lalu buat sebuah file .sh di /home/pi
+# 
+# nano run.sh
+#
+# isinya:
+# cd /home/pi/*namafolderaplikasi*
+# python3 main.py
